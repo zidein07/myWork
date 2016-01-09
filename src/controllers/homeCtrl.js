96c9cddcd8feb1f92d15565fd14538947cb03cac
zidein07 <zidein07@gmail.com>
2016-01-09 20:28:52 +0400

    upd data

--- src/controllers/homeCtrl.js
+++ src/controllers/homeCtrl.js
@@ -2,6 +2,14 @@ app.controller('HomeCtrl', function ($scope, $http, $location) {
   $scope.breadcrumbsOptions = [{
     name: 'Главная страница'
   }];
+  $scope.pageList = [
+    {
+      name: 'Square animate',
+      slug: 'squareAnimate',
+      img: '/assets/img/squareAnimate.png',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+    }
+  ];
   $scope.goToPage = function (name) {
     var pathName = '/' + name;
     console.log('pathName', pathName);
--- src/controllers/squareAnimateCtrl.js
+++ src/controllers/squareAnimateCtrl.js
@@ -5,4 +5,37 @@ app.controller('squareAnimateCtrl', function ($scope, $http, $location) {
     name: 'Square animate',
     url: 'squareAnimate'
   }];
+  var habitat = $('.habitat');
+  var blockBlue = $('.blockBlue');
+  var blockRed = $('.blockRed');
+
+  var w = habitat.width;
+  var h = habitat.height;
+
+  blockProp = function(speed, size, x, y) {
+    this.speed = speed;
+    this.size = size;
+    this.x = x;
+    this.y = y;
+  };
+
+  var BlockBlueProp = new blockProp(5, 20, 0, 0);
+  var BlockRedProp = new blockProp(5, 20, 0, 0);
+
+  setInterval(function(){
+    BlockBlueProp.y += BlockBlueProp.speed;
+    BlockRedProp.x += BlockRedProp.speed;
+    if (BlockBlueProp.y >= 380 || BlockBlueProp.y < -380) {
+      BlockBlueProp.speed = -BlockBlueProp.speed;
+    }
+    if (BlockRedProp.x >= 380 || BlockRedProp.x < -380) {
+      BlockRedProp.speed = -BlockRedProp.speed;
+    }
+
+    blockRed.css({top: BlockRedProp.x});
+    blockBlue.css({left: BlockBlueProp.y});
+
+  }, 10);
+
+
 });
