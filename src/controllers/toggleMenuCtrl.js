bb9f390b87e8ae96b839bdc9894ea8fdd3f4b287
zidein07 <zidein07@gmail.com>
2016-01-10 15:44:30 +0400

    update 2 work is ready

--- src/controllers/toggleMenuCtrl.js
+++ src/controllers/toggleMenuCtrl.js
@@ -5,4 +5,36 @@ app.controller('toggleMenuCtrl', function ($scope) {
     name: 'Toggle menu',
     url: 'toggleMenu'
   }];
+  var button = $('.button');
+  var blockLeft = $('.toggleMenu-left');
+  var blockRight = $('.toggleMenu-right');
+  var blockBottom = $('.toggleMenu-bottom');
+  var infoMenu = $('.infoMenu');
+
+  blockBottom.find(button).click(function () {
+    if (blockBottom.hasClass('visiBle')){
+      blockBottom.animate({"bottom":"-200px"}, {duration: 300}).removeClass('visiBle');
+    }else{
+      blockBottom.animate({"bottom":"0px"},  {duration: 300}).addClass('visiBle');
+    }
+  });
+  blockLeft.find(button).click(function () {
+    if (blockLeft.hasClass('visiBle')){
+      blockLeft.animate({"left":"-200px"}, {duration: 300}).removeClass('visiBle');
+    }else{
+      blockLeft.animate({"left":"0px"},  {duration: 300}).addClass('visiBle');
+    }
+  });
+  blockRight.find(button).click(function () {
+    if (blockRight.hasClass('visiBle')){
+      blockRight.animate({"right":"-200px"}, {duration: 300}).removeClass('visiBle');
+    }else{
+      blockRight.animate({"right":"0px"},  {duration: 300}).addClass('visiBle');
+    }
+
+  });
+
+  var hideAndShowClass = function () {
+
+  }
 });
